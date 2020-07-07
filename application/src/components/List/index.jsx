3d924fa6bf89a81121b86bdd0287a34a0ab6aa5b
Nemph1sClark <nemph1s.k@gmail.com>
2020-07-07 17:36:06 +0300

    Version 2.2
    Fix

--- application/src/components/List/index.jsx
+++ application/src/components/List/index.jsx
@@ -5,6 +5,7 @@ import Badge from '../Badge' // For use the colors from Badge
 
 import './List.scss';
 
+
 const List = ({ items, isRemovable, onClick }) => {
 	return (
 		<ul onClick={onClick} className="list">
@@ -12,7 +13,7 @@ const List = ({ items, isRemovable, onClick }) => {
 				items.map((item, index) => (
 					<li key={index} className={classNames(item.className, { 'active': item.active })}>
 
-						<i>{ item.icon ? item.icon : <Badge color={item.color} /> }</i>
+						<i>{item.icon ? item.icon : <Badge color={item.color} />}</i>
 						<span>{item.name}</span>
 
 					</li>
